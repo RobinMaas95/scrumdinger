3693dfed573e24066f02e4cd664e7d1632a04a12
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2021-07-16 19:35:56 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Robin Maasjosthusmann on 16.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
